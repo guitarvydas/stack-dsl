6ae73f4341fbb4f3fe2070af6b9267c02b4b5071
Paul Tarvydas <guitarvydas@gmail.com>
2020-04-23 07:09:30 -0400

    rm deprecated files

--- test.lisp
+++
@@ -1,7 +0,0 @@
-(in-package :stack-dsl)
-
-(defun test ()
-  (let ((p (make-instance 'stack-dsl-parser)))
-    (let ((pasm:*pasm-tracing* nil))
-      (let ((r (pasm:transpile p *pasm* *use-dsl* 'stack-language)))
-	r))))
