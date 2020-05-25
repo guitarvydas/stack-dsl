ea3dbc518224a0756ce2740ddc22a4593eef5716
Paul Tarvydas <guitarvydas@gmail.com>
2020-05-25 08:50:44 -0400

    wip: test5

--- json-transpiler.lisp
+++ json-transpiler.lisp
@@ -6,4 +6,11 @@
       (read-line f nil :EOF)
       (@:loop
 	(@:exit-when (eq :EOF line))
-	(cond ((
+	(cond ((register-groups-bind (regs) (pattern-string string)
+				     ...))
+	      ((register-groups-bind 
+
+		 (register-groups-bind (fname lname (#'parse-integer date month year))
+      ("(\\w+)\\s+(\\w+)\\s+(\\d{1,2})\\.(\\d{1,2})\\.(\\d{4})" "Frank Zappa 21.12.1940")
+    (list fname lname (encode-universal-time 0 0 0 date month year 0)))
+("Frank" "Zappa" 1292889600)
--- patternmatch.lisp
+++ patternmatch.lisp
@@ -7,14 +7,18 @@
 (defun exprdsl (string-to-scan)
   (let ((s (make-instance 'string-scanner :text string-to-scan)))
     (@:loop
-      (@:exit-when 
+      (format *standard-output* "~&start = ~a~%" (start s))
+      (@:exit-when (= (length string-to-scan) (start s)))
+      (exprdslparser s))))
 
+(defmethod exprdslparser ((s string-scanner))
     (let ((i (id s)))
       (input s "=")
       (let ((r (cond ((look s "{") (classWithFields s))
 		     ((look s ":") (builtinType s))
-		     ((look s "|") (compositeTypeList s)))))
-	(list i r)))))
+		     ((look s "'") (enumList s))
+		     ((look s "\\|") (compositeTypeList s)))))
+	(list i r))))
 
 (defmethod classWithFields ((s string-scanner))
   (input s "{")
@@ -29,9 +33,13 @@
 	((match s "string") "string")))
 
 (defmethod enumList ((s string-scanner))
-  (cond ((match "|" s)
+  (let ((c (enumConstant s)))
+    (cons c (enumTail s))))
+
+(defmethod enumTail ((s string-scanner))
+  (cond ((match s "|")
 	 (let ((c (enumConstant s)))
-	   (cons c (enumList s))))
+	   (cons c (enumTail s))))
 	(t nil)))
 
 (defmethod compositeTypeList ((s string-scanner))
@@ -50,22 +58,28 @@
 	(t nil)))
 
 (defmethod id ((s string-scanner))
-  (prog2
-      (ws s)
-      (input s "\\w+")
-    (ws s)))
+  (input s "\\w+"))
 
 (defmethod id? ((s string-scanner))
   (look s "\\w"))
 
 (defmethod ws ((s string-scanner))
-  (input s "[ \\t\\n\\r]*")
+  (match s "[ \\t\\n\\r]+")
   nil)
 
 
-
+(defmethod skip ((s string-scanner))
+  (multiple-value-bind (match-start match-end reg-start reg-ends)
+      (cl-ppcre:scan "^[ \\t\\n\\r]+"
+		     (text s)
+		     :start (start s))
+    (declare (ignore reg-start reg-ends))
+    (when match-start
+	(setf (start s) match-end)
+	(format *standard-output* "~a" (subseq (text s) match-start match-end)))))
 
 (defmethod input ((s string-scanner) pattern-string)
+  (skip s)
   (multiple-value-bind (match-start match-end reg-start reg-ends)
       (cl-ppcre:scan (concatenate 'string "^" pattern-string)  
 		     (text s)
@@ -73,18 +87,22 @@
     (declare (ignore reg-start reg-ends))
     (if match-start
 	(setf (start s) match-end)
-	(error (format nil "parse error expecting ~s but got ~s" pattern-string (subseq (text s) (start s) (+ 10 (start s))))))))
+	(error (format nil "parse error expecting ~s but got ~s" pattern-string (subseq (text s) (start s) (min (length (text s)) (+ 10 (start s)))))))
+(format *standard-output* "~a" (subseq (text s) match-start match-end))))
 
 (defmethod look ((s string-scanner) pattern-string)
+  (skip s)
   (multiple-value-bind (match-start match-end reg-start reg-ends)
       (cl-ppcre:scan (format nil "^(?=~a)" pattern-string)
 		     (text s)
 		     :start (start s))
     (declare (ignore match-end reg-start reg-ends))
-    match-start))
+    (and match-start (> match-start (start s)))))
 
 (defmethod match ((s string-scanner) pattern)
-  (and (look s pattern) (input s pattern)))
+  (skip s)
+  (format *standard-output* "~&match ~a look(~s)=~a~%" (start s) pattern (look s pattern))
+  (and (look s pattern) (input s pattern) t))
 
 
 (defun test ()
@@ -100,4 +118,56 @@ name = :string
 "
 	  ))
     (exprdsl str)))
+	  
+(defun test1 ()
+  (let ((str "  "))
+    (let ((s (make-instance 'string-scanner :text str)))
+      (ws s))))
+	  
+(defun test2 ()
+  (let ((str "abc"))
+    (let ((s (make-instance 'string-scanner :text str)))
+      (id s))))
+	  
+(defun test3 ()
+  (let ((str "   abc"))
+    (let ((s (make-instance 'string-scanner :text str)))
+      (id s)
+      s)))
+	  
+(defun test4 ()
+  (let ((str "expression = { ekind object }"))
+    (let ((s (make-instance 'string-scanner :text str)))
+      (exprdslparser s))))
+	  
+(defun test5 ()
+  (let ((str "ekind = 'true' | 'false' | 'object'"))
+    (let ((s (make-instance 'string-scanner :text str)))
+      (exprdslparser s))))
+	  
+(defun test6 ()
+  (let ((str "parameterList =| nameMap | nameMap"))
+    (let ((s (make-instance 'string-scanner :text str)))
+      (exprdslparser s))))
+	  
+(defun test7 ()
+  (let ((str "expression = :string"))
+    (let ((s (make-instance 'string-scanner :text str)))
+      (exprdslparser s))))
+	  
+(defun test8 ()
+  (let ((str "expression = :bag"))
+    (let ((s (make-instance 'string-scanner :text str)))
+      (exprdslparser s))))
+	  
+(defun test9 ()
+  (let ((str "expression = :map"))
+    (let ((s (make-instance 'string-scanner :text str)))
+      (exprdslparser s))))
+	  
+(defun test10 ()
+  (let ((str "expression = { ekind object }
+ekind = 'true' | 'false' | 'object'"))
+    (let ((s (make-instance 'string-scanner :text str)))
+      (exprdslparser s))))
 	  
